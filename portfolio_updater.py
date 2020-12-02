04a4e928fd94efbc511544aafa54348da3bd303d
Marco Andorno <mksoc@users.noreply.github.com>
2020-12-02 17:08:55 +0100

    Added timestamp

--- portfolio_updater.py
+++ portfolio_updater.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
+
 from sheet_handles import PortfolioHandle
 from price_getter import PriceGetter
+from datetime import datetime
 
 def print_price(isin, price):
   if 'US' in isin:
@@ -35,10 +38,18 @@ items = len(isin_list)
 
 # For each entry, retrieve NAV and update sheet
 for index, isin in enumerate(isin_list):
-  print(f'({index + 1}/{items})', end=' ', flush=True)
+  count = f'({index + 1}/{items})'
+  print(f'{count: <7}', end=' ', flush=True)
   row_idx = ws.find(isin).row
   price = pg.price(isin)
   print_price(isin, price)
   ws.update_cell(row_idx, col_idx, price)
+
+# Write datetime of update
+update_cell = ws.find('Ultimo aggiornamento:')
+col_idx = update_cell.col + 1
+row_idx = update_cell.row
+now = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+ws.update_cell(row_idx, col_idx, now)
 
 print('Done.')
