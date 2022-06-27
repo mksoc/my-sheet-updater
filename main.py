eb2236e12b809be40df1f85c1c8cfdb46d356b14
Marco Andorno <Marco@macbook-pro-di-marco.home>
2022-06-27 23:01:13 +0200

    Added menu

--- main.py
+++ main.py
@@ -6,10 +6,23 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
+options = {
+    '1': 'update_au',
+    '2': 'update_portfolio',
+    'a': 'all',
+    'q': 'quit'
+}
 
 def print_with_time(string):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print(f'({timestamp}) {string}')
+
+def print_menu():
+    print()
+    print('Available options:')
+    for key in options:
+        print(f'{key}: {options[key]}')
+    print()
 
 def update_au():
     handle = SheetHandle(config.au_sheet)
@@ -43,5 +56,29 @@ def update_au():
 
     print_with_time('Done updating Au sheet.')
 
-if __name__ == '__main__':
+def update_portfolio():
+    try:
+        with open(config.portfolio, 'r') as portfolio_file:
+            pass
+    except FileNotFoundError as e:
+        print(f'Error: {e}')
+        print('Could not complete portfolio update, check that the Excel file is there.')
+        return
+
+def all():
     update_au()
+    update_portfolio()
+
+def quit():
+    print('Goodbye.')
+    exit()
+
+if __name__ == '__main__':
+    while(True):
+        print_menu()
+        choice = input('Please enter your choice: ')
+        
+        if choice in options.keys():
+            eval(options[choice] + '()')
+        else:
+            print('Invalid option. Please try again.')
